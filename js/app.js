1b919a2f40ecbd073121ae540af7f2dcccce694b
Vishnu Priya Chandra Sekar <vishnupriyachandrasekar@gmail.com>
2018-03-11 16:17:27 -0500

    completed the section4

--- js/app.js
+++ js/app.js
@@ -5,12 +5,17 @@ app.config(function($routeProvider){
 	$routeProvider
 	.when("/", {
 		templateUrl : "views/groceryList.html",
-		controller : "GroceryListController"
+		controller : "HomeController"
 	})
 
 	.when("/addItem", {
 		templateUrl : "views/addItem.html", 
 		controller : "GroceryListController"
+	})
+
+	.when("/addItem/edit/:id", {
+		templateUrl : "views/addItem.html", 
+		controller: "GroceryListController"
 	})
 
 	.otherwise({
@@ -23,11 +28,20 @@ app.service("GroceryService", function(){
    var groceryService = {};
 
    groceryService.groceryItems = [
-		{id: 1, itemsName: 'milk', date: '2014-10-01'},
-		{id: 2, itemsName: 'cookies', date: '2014-10-02'},
-		{id: 3, itemsName: 'ice cream', date: '2012-04-03'},
-		{id: 4, itemsName: 'eggs', date: '2012-08-01'}
+		{id: 1, itemsName: 'milk', completed: true, date: new Date("October 1, 2014 11:12:00")},
+		{id: 2, itemsName: 'cookies',completed: false,  date: new Date("October 3, 2014 11:12:00")},
+		{id: 3, itemsName: 'ice cream',completed: false,  date: new Date("November 1, 2014 11:12:00")},
+		{id: 4, itemsName: 'eggs', completed: true, date: new Date("December 1, 2014 11:12:00")}
 	];
+
+
+	groceryService.findById = function(id){
+		for(var item in groceryService.groceryItems){
+			if(groceryService.groceryItems[item].id === id)
+				return groceryService.groceryItems[item];
+
+		}
+	}
 
    groceryService.createID = function(){
    	// of the newId variable exist then increment by one else create the variable
@@ -44,10 +58,31 @@ app.service("GroceryService", function(){
    }
 
    groceryService.save = function(entry){
-	 entry.id = groceryService.createID();
-   	 groceryService.groceryItems.push(entry);
- };
+   	// We need an approach that could differenitiate edit operation and save operation.
+   	 console.log(" Entry id "+entry.id);
+     var item = groceryService.findById(entry.id);
 
+     if(item){
+     	  item.itemsName = entry.itemsName;
+     	  item.date = entry.date;
+     }
+     else {
+     	  entry.id = groceryService.createID();
+   	 	  groceryService.groceryItems.push(entry);
+     }
+	
+   };
+
+   groceryService.removeItem = function(item){
+   			var index = groceryService.groceryItems.indexOf(item);
+   			groceryService.groceryItems.splice(index,1);
+   			console.log(groceryService.groceryItems);
+   }
+
+
+   groceryService.markCompleted = function(item){
+   	   item.completed = !item.completed;
+   }
 	return groceryService;
 
 
@@ -61,15 +96,31 @@ app.controller("headerController",["$scope", function($scope){
 
 
 app.controller("GroceryListController", ["$scope", "$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService){
-	$scope.groceryItems = GroceryService.groceryItems;
 
-	$scope.itemToBeAdded = {id: 0, itemsName: '', date: new Date()};
+    if(!$routeParams.id) // when the addItem url doesnt have an id => the user is gonna add a new item and click save
+		$scope.itemToBeAdded = {id: 0, itemsName: '', completed: false, date: new Date()};
+	else{ // when addItem url has id parameter => editing the name of the existing item
+		 $scope.itemToBeAdded = GroceryService.findById(parseInt($routeParams.id));
+		 console.log($scope.itemToBeAdded);
+	}
 
 	$scope.save = function (){
+
 		GroceryService.save($scope.itemToBeAdded); // add the item to the groceryItems array in the groceryService
 		$location.path("/"); // return the main page
 	}
 
-	console.log($scope.groceryItems);
 }]);
 
+
+app.controller("HomeController", ["$scope", "GroceryService", function($scope, GroceryService){
+    $scope.groceryItems = GroceryService.groceryItems;
+
+    $scope.removeItem = function(item){
+    		GroceryService.removeItem(item);
+    }
+
+    $scope.markCompleted = function(item){
+    	GroceryService.markCompleted(item);
+    }
+}]);
